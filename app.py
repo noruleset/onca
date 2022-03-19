c7b5f1a9d46cb718e7c113b611ab99c6d3e53923
noruleset <noruleset@pm.me>
2022-03-18 23:27:45 -0300

    skip date added

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import pandas as pd
 from flask import Flask
-from datetime import date
+from datetime import date,timedelta
 
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    dt = date.today().strftime("%d/%-m/%Y")
+@app.route('/<skip>')
+def main(skip=None):
+    today = date.today()
+    if skip:
+        tom = today + timedelta(days = 1) 
+        dt = tom.strftime("%d/%-m/%Y")
+    else:
+        dt = today.strftime("%d/%-m/%Y")
+
     db = pd.read_csv('oncadb.csv')
     db = db[db['date'] == dt]
     db = db.head().to_dict()
     resp = {'oncall': db}
-
+    
     return resp
 
 if __name__ == '__main__':
