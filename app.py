bf136398acebff63586e987845f29fcbd9324fdb
noruleset <noruleset@pm.me>
2022-04-02 19:06:39 -0300

    fixed date format

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ def main(path=None):
     today = date.today()
     if path == 'skip':
         tom = today + timedelta(days = 1)
-        dt = tom.strftime("%d/%-m/%Y")
+        dt = tom.strftime("%-d/%-m/%Y")
     elif path == 'back':
         yestd = today - timedelta(days = 1)
-        dt = yestd.strftime("%d/%-m/%Y")
+        dt = yestd.strftime("%-d/%-m/%Y")
     else:
-        dt = today.strftime("%d/%-m/%Y")
-
+        dt = today.strftime("%-d/%-m/%Y")
+    
     db = pd.read_csv('oncadb.csv')
     db = db[db['date'] == dt]
     db = db.head().to_dict()
     resp = {'oncall': db}
-    
+        
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
